c1cb5dd3384aefc20bf36597d738b5a1d1683eaf
JoaoOtavioS <contato@joaootavios.com.br>
2020-08-03 01:54:00 -0300

    Otimização geral, melhoria no código inteiro.
    Adicionado diversos comentários úteis, melhorias básicas no código.
    Qualquer erro reportar diretamente para contato@joaootavios.com.br

--- index.js
+++ index.js
@@ -3,63 +3,57 @@ const client = new discord.Client();
 const fs = require("fs");
 const request = require("request");
 const VirusTotalApi = require('virustotal-api')
-const virusTotal = new VirusTotalApi('e1c6cf3d3ae12d46318bb15802d3a596c7ff4e43da9faea362db20daa66307bf')
+// Coloque a sua API do virustotal logo aí, essa abaixo é um exemplo. (não vai funcionar) (this is a example, not work)
+const virusTotal = new VirusTotalApi('as9d9ua9dsa9u8d9a8s98dsa89udu8a9sd9ua9uda9u9ud9au8du98')
 
-client.on('ready', () =>{console.log("O bot foi iniciado.")});
+client.on('ready', () =>{ 
+	console.log("O bot foi iniciado.");
+});
 
 client.on("message", (message) => {
 
-	if (message.author.bot) return; if (message.channel.type === "dm") return;
-	if (message.attachments.first()) {
-	if (message.attachments.first().size > 0) {
-	console.log("Analisando quantia de bytes no arquivo..")
-	console.log(message.attachments.first().size + " bytes detectados..")
+	if (message.author.bot || message.channel.type === "dm") return;
+	if (message.attachments.first() && message.attachments.first().size > 0) {
 
-	const linkfoda = message.attachments.array()[0].url; 
-	const tempName = linkfoda.split("/");
+	const urlarchive = message.attachments.array()[0].url; 
+	const tempName = urlarchive.split("/");
 	const attachName = tempName[tempName.length-1]
 	const l = attachName;
-	const member = message.member; 
 
-	function download(url){ request.get(url).pipe(fs.createWriteStream(attachName));}
+	function download(url){ require("request").get(url).pipe(fs.createWriteStream(attachName));}
 		
 	if (l.lenght <= 0) return;	
-	if (l.includes(".jpeg") || l.includes(".png") || l.includes(".jpg") || l.includes(".gif") || l.includes(".txt") || l.includes(".mp4") || l.includes(".mp3") || l.includes(".mkv")) {return;}
-	if (l.includes(".JPEG") || l.includes(".PNG") || l.includes(".JPG") || l.includes(".GIF") || l.includes(".TXT") || l.includes(".MP4") || l.includes(".MP3") || l.includes(".MKV")) {return;}
+	if (l.includes(".jpeg") || l.includes(".png") || l.includes(".jpg") || l.includes(".gif") || l.includes(".txt") || l.includes(".mp4") || l.includes(".mp3") || l.includes(".mkv") || l.includes(".JPEG") || l.includes(".PNG") || l.includes(".JPG") || l.includes(".GIF") || l.includes(".TXT") || l.includes(".MP4") || l.includes(".MP3") || l.includes(".MKV")) {return;}
 				
-    	download(linkfoda);			
-    	message.delete();
+    	download(urlarchive);			
+	message.delete().catch(O_o => O_o);
+	
+	// Enviando mensagem, que avisa que o bot está verificando o arquivo.
 	message.channel.send("⚠️ Verificando o arquivo "+ "``"+attachName+"``" +", caso o mesmo for seguro o download será liberado na finalização da análise.").then(mensageminicial => {
     
-	console.log("Verificando o arquivo "+attachName+" aguarde para a conclusão da analise.");
+	let replies = ["30000", "45000", "60000", "120000", "180000", "240000"]
+	let result = Math.floor((Math.random() * replies.length));
 
-		let replies = ["30000", "45000", "60000", "120000", "180000", "240000"]
-		let result = Math.floor((Math.random() * replies.length));
-
-		setTimeout(() => {
+	setTimeout(() => {
 
 		function intervalo() {
 		fs.readFile(attachName, (err, data) => { 
   		if (err) {console.log(`Cannot read file. ${err}`)} else {
 		virusTotal.fileScan(data, 'file.js').then((response) => {
-      		let resource = response.resource
-		
-		virusTotal.fileReport(resource).then((result) => {
+		virusTotal.fileReport(response.resource).then((result) => {
             	if (result.verbose_msg == 'Your resource is queued for analysis') return;
 			if (result.verbose_msg === 'Scan finished, information embedded') {
 			clearInterval(this);
 			if (result.positives > 1 || result.positives === 1) {
-				message.delete();
-				fs.unlinkSync(attachName); //apagar arquivo do HD
-                  		message.channel.send("**(VIRUS)** Arquivo verificado. Download não LIBERADO. (Quem enviou: <@"+message.author.id + "> ["+message.author.id+"])");
-                  		console.log("Arquivo '"+attachName+"' foi verificado (virus) e excluido do HD.");
-                  		mensageminicial.delete();
-                  		return;
+					message.delete().catch(O_o => O_o);
+					fs.unlinkSync(attachName);
+                  	message.channel.send("**(VIRUS)** Arquivo verificado. Download não LIBERADO. (Quem enviou: <@"+message.author.id + "> ["+message.author.id+"])");
+                  	mensageminicial.delete().catch(O_o => O_o);
+                  	return;
 			  } else {
-                		message.channel.send("**(SEGURO)** Arquivo verificado. Download do arquivo:", {files: [linkfoda]});
-				fs.unlinkSync(attachName); //apagar arquivo do HD
-				console.log("Arquivo '"+attachName+"' foi verificado (seguro) e excluido do HD.");
-                		mensageminicial.delete();
+                	message.channel.send("**(SEGURO)** Arquivo verificado. Download do arquivo: (Quem enviou: <@"+message.author.id + "> ["+message.author.id+"])", {files: [urlarchive]});
+				    fs.unlinkSync(attachName);
+                	mensageminicial.delete().catch(O_o => O_o);
 			  }
 			  return;
 			}
@@ -67,7 +61,10 @@ client.on("message", (message) => {
 		}).catch(err => {clearInterval(this); setInterval(intervalo, replies[result]);});
     	}})}
 	setInterval(intervalo, 15000);
-}, 5000); 
-})}}});
 
-client.login('YOUR_BOT_TOKEN_HERE');
+	// Tempo para "iniciar o processo", extremamente recomendavel deixar 5000
+	}, 5000); 
+})}});
+
+// Coloque o "token" de seu bot logo abaixo.
+client.login('Coloque o token aqui.');
